5e563b56c66a492b21174570e37cd3af45fcb6f7
diasaleh <diasaleh@github.com>
2018-04-30 19:17:39 +0000

    da

--- mpu.py
+++ mpu.py
@@ -283,7 +283,7 @@ for i in range(20):
         joint_angles.append(model.predict(np.array([[m, 0]]))[0])
 
     clear_output(wait=True)
-    fig = plt.plot(T, joint_angles)
+   # fig = plt.plot(T, joint_angles)
     #plt.show()
 
 def tilt(x,y):
