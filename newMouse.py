d1840ff8041eb05d92e6f7780e419883824fcb6d
diasaleh <diasaleh@github.com>
2018-05-08 18:43:02 +0000

    da

--- newMouse.py
+++ newMouse.py
@@ -15,11 +15,25 @@ import Adafruit_PCA9685
 import smbus
 import math
 import threading
-
+def scaling(x):
+    OldMax = 1
+    OldMin = -1
+    NewMax = 600
+    NewMin = 150
+    OldValue = x
+    OldRange = (OldMax - OldMin)
+    if (OldRange == 0):
+        NewValue = NewMin
+    else:
+        NewRange = (NewMax - NewMin)  
+        NewValue = (((OldValue - OldMin) * NewRange) / OldRange) + NewMin
+    return (NewValue)
 VID = 0x0
-PID = 0x538
+PID = 0x3825
 DATA_SIZE = 4
-
+learning_episodes = 0
+exitFlag = [0]*learning_episodes
+results = [None] * learning_episodes
 # printina modulio vidurius :for i in dir(usb.util): print i
 
 # try to find Logiceh USB mouse
@@ -45,23 +59,23 @@ endpoint = device[0][(0,0)][0]
 print (endpoint.wMaxPacketSize)
 
 def getMouseData(idd,results):
-    a=[]
-    print (threading.currentThread().getName(), 'Starting '+str(idd))
+	a=[]
+	print (threading.currentThread().getName(), 'Starting '+str(idd))
 
-    data = array.array('B',(0,)*4)
-    while data[0] != 3 and (not exitFlag[idd]):
-        try:
-            data = device.read(endpoint.bEndpointAddress, endpoint.wMaxPacketSize)
-            print (data[2:])
-            a.append(data[2:])    
-        
-        except usb.core.USBError as e:
-            if e.args == ('Operation timed out',):
-                print ("timeoutas")
-                continue
-    results[idd] = a
-    print (threading.currentThread().getName(), 'Exiting '+str(idd))
-    # print ("baigiau! :)")
+	data = array.array('B',(0,)*4)
+	while data[0] != 3 and (not exitFlag[idd]):
+	    try:
+	        data = device.read(endpoint.bEndpointAddress, endpoint.wMaxPacketSize)
+#	        print (data[2:])
+	        a.append(data[2:])    
+	    
+	    except usb.core.USBError as e:
+	        if e.args == ('Operation timed out',):
+	            print ("timeoutas")
+	            continue
+	results[idd] = a
+	print (threading.currentThread().getName(), 'Exiting '+str(idd))
+	# print ("baigiau! :)")
  
  
 pwm = Adafruit_PCA9685.PCA9685()
@@ -84,7 +98,6 @@ def set_servo_pulse(channel, pulse):
 pwm.set_pwm_freq(60)
 
 print('Moving servo on channel 0, press Ctrl-C to quit...')
-
 def servo(action):
     print (action)
     action = list(map(scaling,action))
@@ -93,17 +106,19 @@ def servo(action):
     sleep(.1)
     
 
-
+def servoControl(idd,test_angles,test_angles2):
+    print (threading.currentThread().getName(), 'Starting '+str(idd))
+    for u in range(4):
+	servo(test_angles)
+    	servo(test_angles2)
+    print (threading.currentThread().getName(), 'Exiting '+str(idd))
+    exitFlag[idd] = 1
+    
 action_num = 256
 observation_num = 8
 distance_riq = 0
 
-def servoControl(idd,test_angles,test_angles2):
-    print (threading.currentThread().getName(), 'Starting '+str(idd))
-    servo(test_angles)
-    servo(test_angles2)
-    print (threading.currentThread().getName(), 'Exiting '+str(idd))
-    exitFlag[idd] = 1
+
 def sleep(x):
     time.sleep(x)
     
@@ -115,21 +130,17 @@ sleep(1)
 a=[]
 acc=[]
 for o in range(5):
-    print "hy"
-    servo([0,0,0,0])
+	print "hy"
+	servo([0,0,0,0])
 max_dis = 0
-learning_episodes = 5
-exitFlag = [0]*learning_episodes
-results = [None] * learning_episodes
-
-for z in range(learning_episodes):
-    print(z)
+sum_array=[]
+for i in range(learning_episodes):
+    
     #rd = np.random.randn(8) * 0.3
     #rd[[1,3,5,7]] = 0
     #test_angles = np.clip(s+rd, -1,1)
-    print("\n\n"+str(z)+"\n\n")
+    print("\n\n"+str(i)+"\n\n")
     #for i in np.linspace(0,1,10):
-    
     #    servo((1-i) * last_s + i * test_angles)
     test_angles = np.zeros(4)
     test_angles = np.random.choice([-.25,0,.25],4)
@@ -140,28 +151,29 @@ for z in range(learning_episodes):
     w = threading.Thread(name='servoControl', target=servoControl,args=(i,test_angles,test_angles2))
  #    for u in range(4):
  #        servo(test_angles)
-    # sum1 = 0
-    # for t in range(10):
- #          sum1 += getMotion()
-    # sum1 = sum1/10.0
-    # acc.append(sum1)
+	# sum1 = 0
+	# for t in range(10):
+ #        	sum1 += getMotion()
+	# sum1 = sum1/10.0
+	# acc.append(sum1)
  #        servo(test_angles2)
-    # sum2 = 0
- #      for t in range(10):
+	# sum2 = 0
+ #    	for t in range(10):
  #                sum2 += getMotion()
-    # sum2 = sum2/10.0
+	# sum2 = sum2/10.0
  #        acc.append(sum2)
         #servo(test_angles3)
     w.start()
     t.start()
     w.join()
     t.join()
-    print(results)
+    sum_array.append(np.sum(results[i],axis=0))
+    print(sum_array[i])
     # acc_max = max(acc)
     # cur_distance = distance()
     #last_s = test_angles.copy()
     # div_distance = cur_distance - pre_distance
-    # a.append([acc_max,test_angles,test_angles2])
+    a.append([sum_array[i][0],[test_angles,test_angles2]])
     # if div_distance > max_dis:
     #     max_dis = div_distance
     #     action1_max = test_angles
@@ -169,19 +181,29 @@ for z in range(learning_episodes):
 
     # print(acc_max)
     # acc = []
-    # servo([0,0,0,0])   
-    # sleep(.5)
+    servo([0,0,0,0])   
+    sleep(.5)
 
     #np.savetxt('newangles_m.txt', angles)
     #np.savetxt('newsenses_m.txt', senses)
-#a = sorted(a, key=itemgetter(0),reverse=True)
-#with open('outfileDelay1_acc', 'wb') as fp:
- #  pickle.dump(a, fp)
+#a_sorted = sorted(a, key=itemgetter(0),reverse=True)
+#with open('outfileMouse_results_array', 'wb') as fp:
+#   pickle.dump(results, fp)
+#with open('outfileMouse_a_array', 'wb') as fp:
+#   pickle.dump(a, fp)
+#with open('outfileMouse_sum_array', 'wb') as fp:
+#   pickle.dump(sum_array, fp)
+#print (sum_array)
 
-# with open ('outfileDelay1_acc', 'rb') as fp:
-#       a = pickle.load(fp)
-# a = sorted(a, key=itemgetter(0),reverse=True)
-# # with open('outfile', 'wb') as fp:
+with open ('outfileMouse_sum_array', 'rb') as fp:
+       sum_array1 = pickle.load(fp)
+
+with open ('outfileMouse_a_array', 'rb') as fp:
+       a = pickle.load(fp)
+for c in range(len(a)):
+	a[c][0] = sum_array1[c][1]
+a_sorted = sorted(a, key=itemgetter(0),reverse=True)
+ # with open('outfile', 'wb') as fp:
 # #     pickle.dump(a, fp)
 # with open ('outfile', 'rb') as fp:
 #     itemlist = pickle.load(fp)
@@ -189,26 +211,26 @@ for z in range(learning_episodes):
 
 # print  (a)
 
-# for pp in range(len(a)):
-#     print(pp)
-#     sleep(5)
-#     for p in range(30):
-#   print(a[pp])
-#         #servo(a[len(a)-1][1])
-#         #servo(a[len(a)-1][2])
-#         #servo(a[len(a)-1][3])
-#         servo(a[pp][1])
-#         servo(a[pp][2])
+for pp in range(10):
+     print(pp)
+     sleep(5)
+     for p in range(40):
+ 	print(a_sorted[pp])
+        #servo(a_sorted[len(a_sorted)-1][1])
+        #servo(a[len(a)-1][2])
+        #servo(a[len(a)-1][3])
+        servo(a_sorted[pp][1][0])
+        servo(a_sorted[pp][1][1])
         #servo(a[0][3])
-'''
+
 sleep(3)
 for pp in range(1):
     print(pp)
     sleep(5)
     for p in range(100):
-        print(a[len(a)-1])
-        servo(a[len(a)-1][1])
-        servo(a[len(a)-1][2])
+        print(a_sorted[len(a_sorted)-1])
+        servo(a_sorted[len(a_sorted)-1][1][0])
+        servo(a_sorted[len(a_sorted)-1][1][1])
         # servo(a[len(a)-1][3])
        #  servo(a[0][1])
        #  servo(a[0][2])
@@ -217,7 +239,7 @@ for pp in range(1):
 #angles = np.loadtxt('newangles.txt')
 #senses = np.loadtxt('newsenses.txt')
 
-'''
+
 # a = np.array([ [1, 0, 255, 15, 0, 0] , [1, 0, 255, 31, 0, 0] , [1, 0, 4, 192, 255, 0] , [1, 0, 1, 224, 255, 0] , [1, 0, 4, 240, 255, 0] , [1, 0, 0, 0, 0, 0] , [1, 0, 0, 0, 0, 0] , [1, 0, 0, 0, 0, 0] , [1, 0, 2, 208, 255, 0] , [1, 0, 0, 0, 0, 0] , [1, 0, 0, 0, 0, 0] , [1, 0, 0, 0, 0, 0] , [1, 0, 0, 0, 0, 0] , [1, 0, 0, 224, 255, 0] , [1, 0, 254, 15, 0, 0] , [1, 0, 0, 16, 0, 0] , [1, 0, 0, 32, 0, 0] , [1, 0, 3, 240, 255, 0] , [1, 0, 254, 15, 0, 0] , [1, 0, 0, 0, 0, 0] , [1, 0, 0, 16, 0, 0] , [1, 0, 253, 31, 0, 0] , [1, 0, 255, 255, 255, 0] , [1, 0, 252, 15, 0, 0] , [1, 0, 252, 31, 0, 0] , [1, 0, 255, 15, 0, 0] , [1, 0, 254, 15, 0, 0] , [1, 0, 255, 15, 0, 0] , [1, 0, 0, 0, 0, 0] , [1, 0, 0, 240, 255, 0] , [1, 0, 0, 0, 0, 0] , [1, 0, 1, 0, 0, 0] , [1, 0, 0, 0, 0, 0] , [1, 0, 4, 0, 0, 0] , [1, 0, 255, 31, 0, 0] , [1, 0, 1, 32, 0, 0] , [1, 0, 5, 208, 255, 0] , [1, 0, 0, 0, 0, 0] , [1, 0, 0, 0, 0, 0] , [1, 0, 0, 0, 0, 0] , [1, 0, 0, 240, 255, 0] , [1, 0, 1, 208, 255, 0] , [1, 0, 0, 0, 0, 0] , [1, 0, 251, 175, 0, 0] , [1, 0, 0, 112, 0, 0] , [1, 0, 254, 255, 255, 0] , [1, 0, 0, 240, 255, 0] , [1, 0, 0, 240, 255, 0] , [1, 0, 255, 15, 0, 0] , [1, 4, 0, 0, 0, 0] , [1, 0, 0, 0, 0, 0] , [1, 0, 0, 0, 0, 0] , [1, 0, 0, 0, 0, 0] , [1, 0, 0, 0, 0, 0] , [1, 0, 0, 0, 0, 0] , [1, 0, 0, 0, 0, 0] , [1, 0, 0, 0, 0, 0] , [1, 0, 0, 0, 0, 0] , [1, 0, 0, 0, 0, 0] , [1, 0, 0, 0, 0, 0] , [1, 0, 0, 0, 0, 0] , [1, 0, 0, 240, 255, 0] , [1, 0, 254, 31, 255, 0] , [1, 0, 255, 127, 255, 0] , [1, 0, 255, 159, 0, 0] , [1, 0, 3, 64, 0, 0] , [1, 0, 0, 0, 0, 0] ])
 # aa = np.array([[10,20,30,40],[20,20,70,80]])
 # aaa = [[10,20,30,40],[20,20,70,80]]
